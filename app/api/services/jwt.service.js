318c45d7dcf532973078eabfc47ea0b5206ba52f
ciless <ciless.it@gmail.com>
2019-01-27 20:25:35 +0300

    Fix JWT Expires time

--- app/api/services/jwt.service.js
+++ app/api/services/jwt.service.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
     async genToken(data) {
-        return await jwt.sign(data, process.env.JWT_KEY);
+        return await jwt.sign(data, process.env.JWT_KEY, { expiresIn: '1h' });
     },
 
     async verify(token) {
-        return await jwt.verify(token, process.env.JWT_KEY, { expiresIn: "1h" });
+        return await jwt.verify(token, process.env.JWT_KEY);
     }
 };
